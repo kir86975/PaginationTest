b744955f2733115f9ae2a31c2efbd7733a16b964
Cyril <kir86975@yandex.ru>
2016-11-10 13:00:12 +0500

    - Доработал отображение навигации на странице

--- index.php
+++ index.php
@@ -29,9 +29,45 @@ $paginator->setCurrentPageNumber($page);
 <?php endif; ?>
 
 <?php
-    $pagesInRange = $paginator->getPagesInRange(1, 12);
+    if($page > 1){
+        echo "<a href='index.php?page=" . ($page - 1) . "'> << Previous | </a>";
+    } else{
+        echo '<< Previous | ';
+    }
+
+    $pagesInRange = $paginator->getPagesInRange($page - 2, $page + 2);
+    $rangeKeys = array_keys($pagesInRange);
+    $firstKey = $rangeKeys[0];
+    $lastKey = $rangeKeys[count($rangeKeys) - 1];
+
+    if($pagesInRange[$firstKey] > 1){
+        echo "<a href='index.php?page=1'>1 | </a>";
+    }
+
+    if($pagesInRange[$firstKey] > 2){
+        echo "... |";
+    }
+
     foreach($pagesInRange as $pageNumber){
-        echo "<a href='index.php?page=".$pageNumber."'>".$pageNumber." | </a>";
+        if($pageNumber == $page){
+            echo $page . ' | ';
+        } else{
+            echo "<a href='index.php?page=".$pageNumber."'>".$pageNumber." | </a>";
+        }
+    }
+
+    if ($pagesInRange[$lastKey] < $paginator->count() - 1) {
+        echo "... | ";
+    }
+
+    if ($pagesInRange[$lastKey] < $paginator->count()) {
+        echo "<a href='index.php?page=" . $paginator->count() . "'>" . $paginator->count() . " | </a>";
+    }
+
+    if($page < $paginator->count()) {
+        echo "<a href='index.php?page=" . ($page + 1) . "'> Next >> </a>";
+    } else{
+        echo " Next >> ";
     }
 ?>
 </body>
